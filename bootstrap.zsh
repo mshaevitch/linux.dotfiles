bbcb2153797a4c4ceb69e0a3ba1f6d9c9625008f
Michael Shaevitch <michael.shaevitch@gmail.com>
2024-07-31 20:44:00 -0400

    Created bootstrap.zsh file to bootstrap my ZSH plugins

---
+++ bootstrap.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+# zsh-autosuggestions - https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md#manual-git-clone
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+# fzf - https://github.com/junegunn/fzf?tab=readme-ov-file#using-git
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# fzf-tab - https://github.com/Aloxaf/fzf-tab?tab=readme-ov-file#manual
+git clone https://github.com/Aloxaf/fzf-tab ~/somewhere
