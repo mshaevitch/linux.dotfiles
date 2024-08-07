e472413392db34922e74231b26b55138696d4b4d
Michael Shaevitch <michael.shaevitch@gmail.com>
2024-08-06 22:23:36 -0400

    Updated .zshrc file and replaced Bash bootstrapp file with a Zsh one

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -1,12 +1,16 @@
-#!/bin/bash
-
-# Copy all dotfiles to home folder
-cp -a ~/git/linux.dotfiles/home/.[!.]* ~/ || { echo "Failed to copy dotfiles"; exit 1; }
+#!/bin/zsh
 
 # Set up Neovim
 mkdir -p ~/.config/nvim
 cp ~/git/linux.dotfiles/nvim/init.lua ~/.config/nvim/
 sudo snap install nvim --classic
+
+sudo apt update -y && sudo apt install -y gpg sudo wget curl
+sudo install -dm 755 /etc/apt/keyrings
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo apt update
+sudo apt install -y mise
 
 # p10k - https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#manual
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
@@ -14,11 +18,12 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 # fzf - https://github.com/junegunn/fzf?tab=readme-ov-file#using-git
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 # fzf-tab - https://github.com/Aloxaf/fzf-tab?tab=readme-ov-file#manual
 git clone https://github.com/Aloxaf/fzf-tab ~/.zsh/fzf-tab
 
-# Install Zsh
-sudo apt install -y zsh || { echo "Failed to install Zsh"; exit 1; }
+# Copy all dotfiles to home folder
+cp -a ~/git/linux.dotfiles/home/.[!.]* ~/ || { echo "Failed to copy dotfiles"; exit 1; }
 
 # Set Zsh as default shell
 chsh -s $(which zsh) || { echo "Failed to change default shell"; exit 1; }
